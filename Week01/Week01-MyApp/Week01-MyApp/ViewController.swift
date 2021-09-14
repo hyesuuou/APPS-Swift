ce147402ada965ab5ae9191d8d462d1ed6db4ef9
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-09-14 21:31:06 +0900

    [#1] week1 assignment

---
+++ Week01/Week01-MyApp/Week01-MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week01-MyApp
+//
+//  Created by 김혜수 on 2021/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Week01/Week01-MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+// 주석
+
+// 변수 var
+var year: Int = 2021;
+
+year = 1993;
+
+// 상수 let
+let meterToFeet = 3.28;
+
+var city: String = "Seoul"
+
+var account = 100
+
+// 차에기름
+account = account - 30
+
+// 과자
+account = account - 5
+
+/*
+ 여러 줄
+ 주석
+ */
+
+print("우왕")
+print("Hello World!")
+print(account)
